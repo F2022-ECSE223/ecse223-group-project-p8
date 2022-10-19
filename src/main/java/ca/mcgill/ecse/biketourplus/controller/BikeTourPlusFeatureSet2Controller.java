9d10c913d534532ec4a743b2295ecb488e8504e2
LukeBebee <ljbebee@gmail.com>
2022-10-19 19:28:19 -0400

    Psuedocode for controller (feature set 2)

--- src/main/java/ca/mcgill/ecse/biketourplus/controller/BikeTourPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/biketourplus/controller/BikeTourPlusFeatureSet2Controller.java
@@ -2,22 +2,67 @@ package ca.mcgill.ecse.biketourplus.controller;
 
 import java.sql.Date;
 
-public class BikeTourPlusFeatureSet2Controller {
+// completed by Lukas Bebee (LukeBebee on github)
+public class BikeTourPlusFeatureSet2Controller { 
 
    public static String updateBikeTourPlus(Date startDate, int nrWeeks, int priceOfGuidePerWeek) {
       // TODO Implement the method, return error message (if any) 
       return "Not implemented yet!";
 
-      // hello there how are tyou
+      /* Pseudocode
+       * 
+       * **First check if inputs are valid
+       *  var error = "";
+       * 
+       *  if (nrWeeks < 0) {
+       *    error = "The number of riding weeks must be greater than or equal to zero";
+       *    return error;
+       *  }
+       * 
+       *  if (priceOfGuidePerWeek < 0) {
+       *    error = "The price of guide per week must be greater than or equal to zero";
+       *    return error;
+       *  }
+       * 
+       *  startYear = startDate.year();     ?????
+       *  curYear = currentYear();          ?????
+       *  if (startYear < curYear) {
+       *    error = "The start date cannot be from previous year or earlier";
+       *    return error;
+       *  }
+       * 
+       * 
+       * **If valid (no error returned), apply changes
+       * 
+       *  Attributes of BikeTourPlus object should be changed to input
+       * 
+       *  return "You have successfully implemented the changes";
+       * 
+       */
+      
    }
 
    public static void deleteParticipant(String email) {
+      /* Pseudocode
+       * 
+       * **First find participant
+       *  for each participant p in BTP {
+       *    if (p.getEmail().equals(email)) {         ??????? get email
+       *      p.delete();                              ???????
+       *      return;
+       *    }
+       *  }
+       * 
+       * **If not returned, no such participant
+       *  
+       * 
+       */
+
    }
 
-   // this method does not need to be implemented by a team with five team members
+   // This method is incomplete as we are a team of only five members
    public static void deleteCombo(String name) {
-      //im not doing this one
-
+      // This method is incomplete as we are a team of only five members
    }
 
 }
