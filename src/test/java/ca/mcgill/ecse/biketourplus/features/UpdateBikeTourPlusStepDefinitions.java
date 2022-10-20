891d993014674fc7bdc028ea1e6a70c0b2793a27
LukeBebee <ljbebee@gmail.com>
2022-10-20 15:50:15 -0400

    Updated step definitions

--- src/test/java/ca/mcgill/ecse/biketourplus/features/UpdateBikeTourPlusStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/biketourplus/features/UpdateBikeTourPlusStepDefinitions.java
@@ -13,45 +13,42 @@ import java.util.Map;
 
 public class UpdateBikeTourPlusStepDefinitions {
 
-// add global variable for error
+private String error;
+private BikeTourPlus btp;
 
 
     @Given("the following BikeTourPlus system exists: \\(p8)")
     public void the_following_bike_tour_plus_system_exists_p8(io.cucumber.datatable.DataTable dataTable) {
 
         /* Current situation:
-         * I believe this should work
-         * clear error String each time
+         * Done?
+         * Potentially change from get to constructor (see lines 34 and 51 but I don't think it should be)
          */
 
-
-
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        // clear errors
+        error = "";
         
-        // For other transformations you can register a DataTableType.
 
-        // create btp
-        BikeTourPlus btp = BikeTourPlusApplication.getBikeTourPlus();
+        // create instance of BikeTourPlus
+        btp = BikeTourPlusApplication.getBikeTourPlus(); 
+        // or should it be the following after we get the attribute values from data table: (see line 51 $$)
+        //btp = BikeTourPlus(atartDate, nrWeeks, priceOfGuidePerWeek);
 
-        List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
-
-        
+        // initialize variables that will be used for attributes (null s.t. an error is thrown if the feature file is not giving values)
         String startDateString = null;
         String nrWeeksString = null;
         String priceOfGuidePerWeekString = null;
 
+        // making a list of maps from the data table from the feature file
+        List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
 
-        for (Map<String, String> row : rows) {
+        for (Map<String, String> row : rows) {          // this will only have one iteration as the feature file only gives one row
             startDateString = row.get("startDate");
             nrWeeksString = row.get("nrWeeks");
             priceOfGuidePerWeekString = row.get("priceOfGuidePerWeek");
         }
         
-        // set attributes (can do with constructor)
+        // set attributes (can do with constructor, see line 33 $$))
             // startDate
         Date startDate=Date.valueOf(startDateString);
         btp.setStartDate(startDate);
@@ -63,7 +60,6 @@ public class UpdateBikeTourPlusStepDefinitions {
             // priceOfGuidePerWeek
         int priceOfGuidePerWeek = Integer.parseInt(priceOfGuidePerWeekString);
         btp.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
-
     }
 
     @When("the manager attempts to update the BikeTourPlus information to start date {string}, number of weeks {string}, and price of guide per week {string} \\(p8)")
@@ -72,18 +68,15 @@ public class UpdateBikeTourPlusStepDefinitions {
         /* Current situation:
          * Done
          */
-        
-        // Write code here that turns the phrase above into concrete actions
 
-            // startDate
+        // strings into proper types to call controller
         Date startDate=Date.valueOf(string);
-            // nrWeeks
         int nrWeeks = Integer.parseInt(string2);
-            // priceOfGuidePerWeek
         int price = Integer.parseInt(string3);
 
-        BikeTourPlusFeatureSet2Controller.updateBikeTourPlus(startDate, nrWeeks, price);
-
+        // call controller 
+        // (this is called on controller, not instance, but I am not sure how it knows that there is only one instance of BikeTourPlus, maybe ask TA)
+        BikeTourPlusFeatureSet2Controller.updateBikeTourPlus(startDate, nrWeeks, price); 
     }
 
     @Then("the BikeTourPlus information shall be start date {string}, number of weeks {string}, and price of guide per week {string} \\(p8)")
@@ -91,51 +84,41 @@ public class UpdateBikeTourPlusStepDefinitions {
         String string, String string2, String string3) {
 
         /* Current situation:
-         * Simple
-         * use getters on global instance of BTP
+         * Simple?
+         * ensure we should use getters
+         * assertEqual() stuff
+         * append error messages with messages from feature files??
          */ 
-        //btp.getStartDate();
         
-
-        // Write code here that turns the phrase above into concrete actions
+        // get attributes of instance
+        Date instanceStartDate = btp.getStartDate();
+        int instanceNrWeeks = btp.getNrWeeks();
+        int instancePrice = btp.getPriceOfGuidePerWeek();
         
+        // get desired attributes
+        Date startDate = Date.valueOf(string);
+        int nrWeeks = Integer.parseInt(string2);
+        int price = Integer.parseInt(string3);
 
-        //     // startDate
-        // Date startDate=Date.valueOf(string);
-        //     // nrWeeks
-        // int nrWeeks = Integer.parseInt(string2);
-        //     // priceOfGuidePerWeek
-        // int price = Integer.parseInt(string3);
-
-
-        // btp.setStartDate(startDate);
-        // btp.setNrWeeks(nrWeeks);
-        // btp.setPriceOfGuidePerWeek(price)
+        // assert equalities and check for errors
 
         // assertEqual()
         // error += (insert error here)
 
-
-
-
-        throw new io.cucumber.java.PendingException();
     }
 
     @Then("the system shall raise the error {string} \\(p8)")
     public void the_system_shall_raise_the_error_p8(String string) {
         // Write code here that turns the phrase above into concrete actions
 
-        /*
-         * if error string is not "" we raise an exception
+        /* Current Situation:
          * 
+         * If error string is not "" we raise an exception
+         * 
+         * Check how we raise exception and if we need to return a specific error message
          */
 
 
-
-
-
-
-
-        throw new io.cucumber.java.PendingException();
+    
     }
 }
