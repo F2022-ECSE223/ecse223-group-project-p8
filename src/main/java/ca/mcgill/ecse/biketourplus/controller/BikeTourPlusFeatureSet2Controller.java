4cf1703e3b08f16e2e64dba041d6e113eabf7c77
LukeBebee <ljbebee@gmail.com>
2022-10-24 14:24:54 -0400

    Update (gear association management WIP)

--- src/main/java/ca/mcgill/ecse/biketourplus/controller/BikeTourPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/biketourplus/controller/BikeTourPlusFeatureSet2Controller.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse.biketourplus.controller;
 
 import ca.mcgill.ecse.biketourplus.model.BikeTourPlus;
+import ca.mcgill.ecse.biketourplus.model.Participant;
+import ca.mcgill.ecse.biketourplus.model.User;
+import ca.mcgill.ecse.biketourplus.model.BookedItem;
+import ca.mcgill.ecse.biketourplus.model.BookableItem;
 import ca.mcgill.ecse.biketourplus.application.BikeTourPlusApplication;
 
 import java.sql.Date;
@@ -8,28 +12,32 @@ import java.sql.Date;
 // completed by Lukas Bebee (LukeBebee on github)
 public class BikeTourPlusFeatureSet2Controller { 
 
+   /**
+    * @param startDate start date of the biking season
+    * @param nrWeeks number of weeks the biking season will last
+    * @param priceOfGuidePerWeek the weekly cost of a guide
+    * @return error message (empty string if no error)
+    * @author LukeBebee
+    */
    public static String updateBikeTourPlus(Date startDate, int nrWeeks, int priceOfGuidePerWeek) {
-      // TODO Implement the method, return error message (if any) 
-      
       var error = "";
-      
-      if (nrWeeks < 0) {
+
+      // check to ensure the three inputs are possible with given constraints
+      if (nrWeeks < 0) { // check number of weeks
          error = "The number of riding weeks must be greater than or equal to zero";
          System.out.println(error);
          return error;
       }
-      
-      if (priceOfGuidePerWeek < 0) {
+      if (priceOfGuidePerWeek < 0) { // check guide price
          error = "The price of guide per week must be greater than or equal to zero";
          System.out.println(error);
          return error;
       }
-      
       // get current date
       long millis=System.currentTimeMillis();  
       java.sql.Date curDate = new java.sql.Date(millis);
 
-      if (curDate.after(startDate)) {
+      if (curDate.after(startDate)) { // check start date against current date
          error = "The start date cannot be from previous year or earlier";
          System.out.println(error);
          return error;
@@ -41,25 +49,32 @@ public class BikeTourPlusFeatureSet2Controller {
       btp.setNrWeeks(nrWeeks);
       btp.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
 
-      return error;
+      return error; // return value will be an empty string if any errors detected
    }
 
+   /**
+    * This method removes a participant from BikeTourPlus
+    * When doing so, it removes all associations that the participant has
+    * @param email
+    * @author LukeBebee
+    */
    public static void deleteParticipant(String email) {
-      /* Pseudocode
-       * 
-       * **First find participant
-       *  for each participant p in BTP {
-       *    if (p.getEmail().equals(email)) {         ??????? get email
-       *      p.delete();                              ???????
-       *      return;
-       *    }
-       *  }
-       * 
-       * **If not returned, no such participant
-       *  
-       * 
-       */
+      BikeTourPlus btp = BikeTourPlusApplication.getBikeTourPlus();
+      User u = User.getWithEmail(email);
+      if (u instanceof Participant) { // Ensure user is a participant
+         Participant p = (Participant) u;
 
+         // Manage booked gear         ***Currently, only not successful with this part
+         for (BookedItem gear : p.getBookedItems()) {
+            //p.removeBookedItem(gear);  // Remove BookedItem from association with participant    // not needed because both instances deleted
+            gear.getItem().removeBookedItem(gear); // From perspective of instance of BookableItem, remove association with BookedItem
+            gear.delete(); // Delete the instance of the association class BookedItem
+         }
+
+         btp.removeParticipant(p); // Remove association with instance of BikeTourPlus
+         p.delete(); // Delete instance of participant
+         return;
+      }
    }
 
    // This method is incomplete as we are a team of only five members
