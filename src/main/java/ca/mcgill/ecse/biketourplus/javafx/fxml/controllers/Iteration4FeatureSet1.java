8548a285037511e7f0f1bed6c9310d1e5a207d8c
LukeBebee <ljbebee@gmail.com>
2022-11-27 13:23:57 -0500

    quick fixes

--- src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet1.java
+++ src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet1.java
@@ -2,7 +2,12 @@ package ca.mcgill.ecse.biketourplus.javafx.fxml.controllers;
 
 import ca.mcgill.ecse.biketourplus.controller.*;
 
-private class Iteration4FeatureSet1 {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Iteration4FeatureSet1 {
     // based on IDs in SceneBuilder
     @FXML
     private TextField inputPasswordTextField;
@@ -12,7 +17,7 @@ private class Iteration4FeatureSet1 {
     // event listeners on buttons (onAction in SceneBuilder)
     @FXML
     public void updatePasswordClicked(ActionEvent event) {
-        BikeTourPlusFeatureSet1Controller.updateManager(inputPasswordTextField);
+        BikeTourPlusFeatureSet1Controller.updateManager(inputPasswordTextField.getText());
     }
 
 
