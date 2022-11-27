9bf3e1be08eddd1ab0bede052c67b899c6c43bb4
LukeBebee <ljbebee@gmail.com>
2022-11-27 13:16:09 -0500

    first draft of view controller and view page

--- src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet1.java
+++ src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet1.java
@@ -1,1 +1,19 @@
 package ca.mcgill.ecse.biketourplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.biketourplus.controller.*;
+
+private class Iteration4FeatureSet1 {
+    // based on IDs in SceneBuilder
+    @FXML
+    private TextField inputPasswordTextField;
+    @FXML
+    private Button updatePasswordButton;
+
+    // event listeners on buttons (onAction in SceneBuilder)
+    @FXML
+    public void updatePasswordClicked(ActionEvent event) {
+        BikeTourPlusFeatureSet1Controller.updateManager(inputPasswordTextField);
+    }
+
+
+}
