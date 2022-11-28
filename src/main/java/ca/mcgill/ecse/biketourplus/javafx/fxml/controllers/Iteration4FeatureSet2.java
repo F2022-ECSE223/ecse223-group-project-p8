a8fb39333fd7b824ffa72e6c1f565ad283731abd
LukeBebee <ljbebee@gmail.com>
2022-11-27 21:02:03 -0500

    feature set 2 controller skeleton

---
+++ src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet2.java
@@ -0,0 +1,54 @@
+package ca.mcgill.ecse.biketourplus.javafx.fxml.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Iteration4FeatureSet2 {
+
+    @FXML
+    private TextField emailParticipantRemoveTextField;
+
+    @FXML
+    private TextField newGuidePriceTextField;
+
+    @FXML
+    private TextField newNrWeeksTextField;
+
+    @FXML
+    private TextField newStartDateTextField;
+
+    @FXML
+    private Button removeParticipantButton;
+
+    @FXML
+    private Button updateGuidePriceButton;
+
+    @FXML
+    private Button updateNrWeeksButton;
+
+    @FXML
+    private Button updateStartDateButton;
+
+    @FXML
+    void removeParticipantClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateGuidePriceClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateNrWeeksClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateStartDateClicked(ActionEvent event) {
+
+    }
+
+}
