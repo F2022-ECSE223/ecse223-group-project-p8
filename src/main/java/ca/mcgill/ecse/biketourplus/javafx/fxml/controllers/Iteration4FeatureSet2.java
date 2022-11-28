694b27032697632e6f83281f89efa513df1969de
LukeBebee <ljbebee@gmail.com>
2022-11-27 21:43:48 -0500

    some progress, MVC pattern issues

--- src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet2.java
+++ src/main/java/ca/mcgill/ecse/biketourplus/javafx/fxml/controllers/Iteration4FeatureSet2.java
@@ -1,54 +1,63 @@
 package ca.mcgill.ecse.biketourplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.biketourplus.controller.*;
+import ca.mcgill.ecse.biketourplus.application.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Iteration4FeatureSet2 {
+  BikeTourPlus btp = BikeTourPlusApplication.getBikeTourPlus();
+  
+  @FXML
+  private TextField emailParticipantRemoveTextField;
 
-    @FXML
-    private TextField emailParticipantRemoveTextField;
+  @FXML
+  private TextField newGuidePriceTextField;
 
-    @FXML
-    private TextField newGuidePriceTextField;
+  @FXML
+  private TextField newNrWeeksTextField;
 
-    @FXML
-    private TextField newNrWeeksTextField;
+  @FXML
+  private TextField newStartDateTextField;
 
-    @FXML
-    private TextField newStartDateTextField;
+  @FXML
+  private Button removeParticipantButton;
 
-    @FXML
-    private Button removeParticipantButton;
+  @FXML
+  private Button updateGuidePriceButton;
 
-    @FXML
-    private Button updateGuidePriceButton;
+  @FXML
+  private Button updateNrWeeksButton;
 
-    @FXML
-    private Button updateNrWeeksButton;
+  @FXML
+  private Button updateStartDateButton;
 
-    @FXML
-    private Button updateStartDateButton;
-
-    @FXML
-    void removeParticipantClicked(ActionEvent event) {
-
+  @FXML
+  void removeParticipantClicked(ActionEvent event) {
+    String email = emailParticipantRemoveTextField.getText();
+    try {
+      BikeTourPlusFeatureSet2Controller.deleteParticipant(email);
+    } catch (InvalidInputException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+  }
 
-    @FXML
-    void updateGuidePriceClicked(ActionEvent event) {
+  @FXML
+  void updateGuidePriceClicked(ActionEvent event) {
+    BikeTourPlusFeatureSet2Controller.updateBikeTourPlus();
+  }
 
-    }
+  @FXML
+  void updateNrWeeksClicked(ActionEvent event) {
 
-    @FXML
-    void updateNrWeeksClicked(ActionEvent event) {
+  }
 
-    }
+  @FXML
+  void updateStartDateClicked(ActionEvent event) {
 
-    @FXML
-    void updateStartDateClicked(ActionEvent event) {
-
-    }
-
+  }
 }
