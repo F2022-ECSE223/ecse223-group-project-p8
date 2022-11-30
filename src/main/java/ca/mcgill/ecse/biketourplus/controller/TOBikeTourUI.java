20820d0697b1362c89b85fd86d71df9660fa717b
Jack Zaarour <jwzaarour@gmail.com>
2022-11-30 01:39:00 -0500

    added tobiketourui

---
+++ src/main/java/ca/mcgill/ecse/biketourplus/controller/TOBikeTourUI.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse.biketourplus.controller;
+import java.util.*;
+
+public class TOBikeTourUI
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TOBikeTour Attributes
+  private int id;
+  private String guideName;
+  private int startWeek;
+  private int endWeek;
+  private String participants;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TOBikeTourUI(int aId, String aGuideName, int aStartWeek, int aEndWeek,  String apaticipants)
+  {
+    id = aId;
+    startWeek = aStartWeek;
+    endWeek = aEndWeek;
+    guideName = aGuideName;
+    participants = apaticipants;
+  }
+}
